eabfaba3a399c388068984a7e75f036b1b1e403a
diayan <diayans@outlook.com>
2022-03-15 19:07:48 +0000

    create viewcontrollers where the dependencies will be used

---
+++ HardDependencies/OverrideViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OverrideViewController.swift
+//  HardDependencies
+//
+//  Created by diayan siat on 14/03/2022.
+//
+
+import UIKit
+
+class OverrideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
